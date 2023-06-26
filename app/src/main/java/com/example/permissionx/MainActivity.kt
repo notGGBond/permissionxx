12156607bbacd8ddf0e3b2b093c7c77a96a4efe9
ZWK <2231825404@qq.com>
2023-06-26 11:24:55 +0800

    开源库

---
+++ app/src/main/java/com/example/permissionx/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.example.permissionx
+
+import android.Manifest
+import android.content.Intent
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import com.example.permissionxlibrary.PermissionX
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val makeCallBtn: Button =findViewById(R.id.makeCallBtn)
+        makeCallBtn.setOnClickListener {
+            PermissionX.request(this, //申请权限传入这里
+                Manifest.permission.CALL_PHONE) { allGranted, deniedList ->
+                if (allGranted) {
+                    call()
+                } else {
+                    Toast.makeText(this, "You denied $deniedList", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+
+    private fun call() {
+        try {
+            val intent = Intent(Intent.ACTION_CALL)
+            intent.data = Uri.parse("tel:10086")
+            startActivity(intent)
+        } catch (e: SecurityException) {
+            e.printStackTrace()
+        }
+    }
+
+}
